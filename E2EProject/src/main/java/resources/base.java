c85a6e97a5e54ccaf472e1fb27bbdbf153ec99d8
kunal <kunalmalpure9991@gmail.com>
2021-04-24 22:25:55 +0530

    Commiting selenium project

---
+++ E2EProject/src/main/java/resources/base.java
@@ -0,0 +1,64 @@
+package resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class base {
+
+	public  WebDriver driver;
+	public Properties prop;
+public WebDriver initializeDriver() throws IOException
+{
+	
+ prop= new Properties();
+FileInputStream fis=new FileInputStream("C:\\Users\\rahul\\E2EProject\\src\\main\\java\\resources\\data.properties");
+
+prop.load(fis);
+String browserName=prop.getProperty("browser");
+System.out.println(browserName);
+
+if(browserName.equals("chrome"))
+{
+	 System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+	driver= new ChromeDriver();
+		//execute in chrome driver
+	
+}
+else if (browserName.equals("firefox"))
+{
+	 driver= new FirefoxDriver();
+	//firefox code
+}
+else if (browserName.equals("IE"))
+{
+//	IE code
+}
+
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+return driver;
+
+
+}
+
+
+public String getScreenShotPath(String testCaseName,WebDriver driver) throws IOException
+{
+	TakesScreenshot ts=(TakesScreenshot) driver;
+	File source =ts.getScreenshotAs(OutputType.FILE);
+	String destinationFile = System.getProperty("user.dir")+"\\reports\\"+testCaseName+".png";
+	FileUtils.copyFile(source,new File(destinationFile));
+	return destinationFile;
+
+
+}
+
+}
